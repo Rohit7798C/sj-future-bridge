05865ad4a7334b62b4f1b014f621da889b521a27
Rohit Rajkumar Chauhan <114377886+Rohit7798C@users.noreply.github.com>
2025-10-20 23:15:32 +0530

    Update supportSchema.py

--- future_bridge/schema/supportSchema.py
+++ future_bridge/schema/supportSchema.py
@@ -1,5 +1,10 @@
-from pydantic import BaseModel, EmailStr
-from typing import Optional, Dict, Any
+from pydantic import BaseModel, EmailStr, Field
+from typing import List, Optional, Dict, Any
+
+from future_bridge.models.supportModel import BulkAction, TicketStatus
+
+from datetime import datetime
+
 
 class SupportRequest(BaseModel):
     """
@@ -18,3 +23,60 @@ class SupportResponse(BaseModel):
     message: str
     success: bool
     data: Dict[str, Any] 
+
+class ExportTicketsRequest(BaseModel):
+    """
+    Schema for filtering/exporting support tickets.
+    """
+    status: Optional[TicketStatus] = Field(None, description="Filter by ticket status (Open, Closed, InProgress, InDevelopment)")
+    ticket_ids: Optional[List[str]] = Field(None, description="List of ticket IDs to export")
+
+class BulkActionRequest(BaseModel):
+    """
+    Schema for performing bulk actions on tickets.
+    """
+    action: BulkAction
+    ticket_ids: List[str]
+
+class TicketFilterRequest(BaseModel):
+    """
+    Schema for filtering tickets in pagination API.
+    """
+    status: Optional[TicketStatus] = None
+    sort: Optional[str] = "created_at:desc"
+    page: int = 1
+    limit: int = 10
+
+
+class CommentRequest(BaseModel):
+    """
+    Schema for user adding comment on a ticket
+    """
+    ticket_id: str
+    comment: str
+
+
+class CommentResponse(BaseModel):
+    """
+    Response after user adds a comment
+    """
+    message: str
+    success: bool
+    data: Dict[str, Any]
+
+
+class AdminCommentRequest(BaseModel):
+    """
+    Schema for admin adding comment on a ticket
+    """
+    ticket_id: str
+    comment: str
+
+
+class AdminCommentResponse(BaseModel):
+    """
+    Response after admin adds a comment
+    """
+    message: str
+    success: bool
+    data: Dict[str, Any]
